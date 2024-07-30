8b5a56c312e94d43d5e294ed08faf6e91d377889
felix-nack <felix.nack@tutanota.com>
2024-07-30 14:09:11 +0200

    Requirements

--- ada/environments/production_utils.py
+++ ada/environments/production_utils.py
@@ -40,39 +40,42 @@ def determine_packaging(env, schedule):
 	zfin_data = env.zfin_data
 	demand = env.get_orders()
 	demand['package'] = demand['zfin'].map(lambda x: env.zfin_data[x][3])
+	
 	for n in demand['doc_num']:
-	    order_data = demand.loc[demand['doc_num']==n]
-	    lot_size = zfin_data[order_data['zfin'].values[0]][4]
-	    pack_type = order_data['package'].values[0]
-	    lot_type = pack_type + '_qty_remaining'
-	    qty = order_data['order_qty'].values[0]
-	    # Fill value from inventory first
-	    if zfin_inventory_temp[
-	        zfin_data[order_data['zfin'].values[0]][-1]] >= order_data['order_qty'].values[0]:
-	        zfin_inventory_temp[zfin_data[order_data['zfin']][-1]] -= order_data['order_qty']
-	    # Assign capacity
-	    else:
-	        # Get matching indices for product availability
-	        idxs = schedule.loc[
-	            (schedule['qty_remaining']>=qty) &
-	            (schedule['gmid']==order_data['gmid'].values[0])].index
-	        # Check lot sizes first for bags and ss
-	        for idx in idxs:
-	            if pack_type != 'bulk':
-	                current_lot = schedule.loc[idx, lot_type]
-	                if current_lot < qty:
-	                    if schedule.loc[idx, 'qty_remaining'] < lot_size:
-	                        # No material to assign, move to next entry in schedule
-	                        continue
-	                    elif schedule.loc[idx, 'qty_remaining'] >= lot_size:
-	                        # Assign a lot to the packaging type
-	                        schedule.loc[idx, lot_type] += lot_size - qty
-	                        schedule.loc[idx, pack_type] += lot_size
-	                        schedule.loc[idx, 'qty_remaining'] -= lot_size
-	                        break
-	                elif current_lot >= qty:
-	                    schedule.loc[idx, lot_type] -= qty
-	                    break
+		order_data = demand.loc[demand['doc_num'] == n]
+		lot_size = zfin_data[order_data['zfin'].values[0]][4]
+		pack_type = order_data['package'].values[0]
+		lot_type = pack_type + '_qty_remaining'
+		qty = order_data['order_qty'].values[0]
+		
+		# Fill value from inventory first
+		if zfin_inventory_temp[zfin_data[order_data['zfin'].values[0]][-1]] >= order_data['order_qty'].values[0]:
+			zfin_inventory_temp[zfin_data[order_data['zfin'].values[0]][-1]] -= order_data['order_qty'].values[0]
+		# Assign capacity
+		else:
+			# Get matching indices for product availability
+			idxs = schedule.loc[
+				(schedule['qty_remaining'] >= qty) &
+				(schedule['gmid'] == order_data['gmid'].values[0])
+			].index
+			
+			# Check lot sizes first for bags and ss
+			for idx in idxs:
+				if pack_type != 'bulk':
+					current_lot = schedule.loc[idx, lot_type]
+					if current_lot < qty:
+						if schedule.loc[idx, 'qty_remaining'] < lot_size:
+							# No material to assign, move to next entry in schedule
+							continue
+						elif schedule.loc[idx, 'qty_remaining'] >= lot_size:
+							# Assign a lot to the packaging type
+							schedule.loc[idx, lot_type] += lot_size - qty
+							schedule.loc[idx, pack_type] += lot_size
+							schedule.loc[idx, 'qty_remaining'] -= lot_size
+							break
+					elif current_lot >= qty:
+						schedule.loc[idx, lot_type] -= qty
+						break
 
 	# Assign unallocated quantities to bulk
 	schedule['bulk'] = schedule['bulk'] + schedule['qty_remaining']
@@ -82,6 +85,7 @@ def determine_packaging(env, schedule):
 	schedule.bulk = schedule.bulk.map(lambda x: np.round(x, 1))
 	schedule.bag = schedule.bag.map(lambda x: np.round(x, 1))
 	schedule.ss = schedule.ss.map(lambda x: np.round(x, 1))
+	
 	return schedule
 
 # def determine_packaging(schedule):
